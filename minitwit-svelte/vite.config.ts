432414353ee970358ba690fc26f195de96ebaeac
Corby7 <corbijn.bulsink@hotmail.com>
2026-02-16 08:18:59 +0000

    Setup docker for Svelte

--- minitwit-svelte/vite.config.ts
+++ minitwit-svelte/vite.config.ts
@@ -5,19 +5,14 @@ export default defineConfig({
   plugins: [sveltekit()],
   server: {
     proxy: {
-      // 1. Intercept anything starting with '/api'
       '/api': {
-        // 2. Where the Java server is actually hiding
-        target: 'http://localhost:7070',
-        
-        // 3. Tells the Java server the request came from its own origin
-        // (Helps with some strict security filters)
+        target: 'http://java-backend:7070',
         changeOrigin: true,
-        
-        // 4. OPTIONAL: If your Java endpoints DON'T have "/api" in them 
-        // (e.g., Java is just /msgs), this removes "/api" before sending.
-        // rewrite: (path) => path.replace(/^\/api/, '')
       }
+    },
+    watch: {
+      usePolling: true,
+      interval: 100   // Poll every 100ms
     }
   }
 });

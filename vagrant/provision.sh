b7bdbfb88a282bed25b231e64cf70d8b774104e3
Corby7 <corbijn.bulsink@hotmail.com>
2026-02-20 13:19:40 +0000

    Moved to vagrant folder and put scripts into own files

---
+++ vagrant/provision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Updating packages..."
+
+# Install dependencies
+apt-get update -y
+apt-get install -y ca-certificates curl gnupg git
+# Add Docker's official GPG key and repo
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list
+# Install Docker + Compose plugin
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "Docker installation complete!"
+
+echo "Starting Docker..."
+
+systemctl enable --now docker
+usermod -aG docker vagrant # allow us to execute docker commands, without using sudo
+
+echo "Cloning and starting app..."
+git clone https://github.com/ZeroDownTime-ITU/minitwit_project.git /minitwit_project
+
+cd /minitwit_project/minitwit-java
+docker compose up -d
+
+echo "Provisioning finished! App is starting in the background."
---
+++ vagrant/provision_keys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJJ6uOxzj07JWt/8aqbwkH3qMIu615PfU0ZvZCHkovi magnusbergstedt@Mac" >> /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6lS3hLYIvcWHKP3zsh2K6SZBOQJWNwBQspdptT8/Fq mono@monolith" >> /root/.ssh/authorized_keys
+# CHANGE THIS TO THE NEW SSH
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOnNkEKfn3FGWFV4W8X39RzFMtF9bYUATHtHGpINPIH3 luismilanengel@Luis-MacBook-Air.local" >> /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmDcJ55KcT1ZRW5200BqocDh8kO3jq6xOzXdEZNCl9V conta@MSI" >> /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2cG2XUbHvk8i7Hmv4ix2KTiM5GAKEbJltl3pvNmCpE jbul@itu.dk" >> /root/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZmlyxqe1cHR+mt1bvPTK/qy0xhaW/AuCjeq9SvfOi
+fshImR/9JhtiT8mo+N7hmTA+xrFxe44NGqKyEwBDCmSaAH8nQRWe1KLlvWGjnfB5m4aNMywhQYmBw04uCwjzTQ
+2EfSJzZxqW2+Daca3GkmxvlmMvrC9+M+Dn+XgF2DgJ3SrJyLW6K8m9MZ8PwKDooyEcNktkv+fDpDYDpyV4X7eW
+WRCmVZG1urw0phLGnhAzP3Z5qvPxr0ETuA2ll499eiOmCoa1EulowjLKhUi/5EFckEBVfJd27fZFZwcxxzNuTL
+fD0WO0IzLm8BdZRETnMGHxSRl5nHh81G+4e8fV6vhqhp/TObECH7PV5U3Gv3KdGEY1a5EhfN62a//CU/7mxc1s
+vxs0GwJc0VOf0h5r6PWoyfC0qZNOlSA3Kg7FdctXJku2ML62+798Qz9NjMeC0LpyBJpryaa985rplRWTX2rmx5
+AHHA+PF3UYJav3Xup9qj6iKX8fUu7OUcNNUqUbQhC5y/X/ulIYBFI9NppJnA8vCIdRyR3gpAsQdDhXC7kaJ69W
+F5XGF9QlqZa6MixKFutVcF52KKdhQgU2E4eupzmNcZC5mv2/EvDtMx86ILtLviX+G/zTKJ8VGXSNsNpHogh49O
+cfrgWc4R01m4Mgy9v/Vu7afxhkOqh+sGFaAhXuQw==                                              mathiassogaard@MacBook-Air-tilhrende-Mathias.local" >> /root/.ssh/authorized_keys

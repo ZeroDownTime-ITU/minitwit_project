16844a3e05519d1d170637327e64915e90e3af9d
DespoTheAnimal <thedanishmagnus@gmail.com>
2026-02-20 15:12:08 +0100

    change for ssh keys and comments for vagrantfile

--- vagrant/provision_keys.sh
+++ vagrant/provision_keys.sh
@@ -5,12 +5,5 @@ echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6lS3hLYIvcWHKP3zsh2K6SZBOQJWNwBQspdp
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPE68oskLFw2fC14xIWiZSIA0veODVn42LeswDDWI0R1 luismilanengel@Luis-MacBook-Air.local" >> /root/.ssh/authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmDcJ55KcT1ZRW5200BqocDh8kO3jq6xOzXdEZNCl9V conta@MSI" >> /root/.ssh/authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2cG2XUbHvk8i7Hmv4ix2KTiM5GAKEbJltl3pvNmCpE jbul@itu.dk" >> /root/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZmlyxqe1cHR+mt1bvPTK/qy0xhaW/AuCjeq9SvfOi
-fshImR/9JhtiT8mo+N7hmTA+xrFxe44NGqKyEwBDCmSaAH8nQRWe1KLlvWGjnfB5m4aNMywhQYmBw04uCwjzTQ
-2EfSJzZxqW2+Daca3GkmxvlmMvrC9+M+Dn+XgF2DgJ3SrJyLW6K8m9MZ8PwKDooyEcNktkv+fDpDYDpyV4X7eW
-WRCmVZG1urw0phLGnhAzP3Z5qvPxr0ETuA2ll499eiOmCoa1EulowjLKhUi/5EFckEBVfJd27fZFZwcxxzNuTL
-fD0WO0IzLm8BdZRETnMGHxSRl5nHh81G+4e8fV6vhqhp/TObECH7PV5U3Gv3KdGEY1a5EhfN62a//CU/7mxc1s
-vxs0GwJc0VOf0h5r6PWoyfC0qZNOlSA3Kg7FdctXJku2ML62+798Qz9NjMeC0LpyBJpryaa985rplRWTX2rmx5
-AHHA+PF3UYJav3Xup9qj6iKX8fUu7OUcNNUqUbQhC5y/X/ulIYBFI9NppJnA8vCIdRyR3gpAsQdDhXC7kaJ69W
-F5XGF9QlqZa6MixKFutVcF52KKdhQgU2E4eupzmNcZC5mv2/EvDtMx86ILtLviX+G/zTKJ8VGXSNsNpHogh49O
-cfrgWc4R01m4Mgy9v/Vu7afxhkOqh+sGFaAhXuQw==                                              mathiassogaard@MacBook-Air-tilhrende-Mathias.local" >> /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMW7+/L+y8iZqzIMKEDcu+WY1Jq5AtIW7a//OhTknoa mathias@minitwit" >> /root/.ssh/authorized_keys
+ 

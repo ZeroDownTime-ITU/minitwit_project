39545eaf0b7162b5f1de8b47a43d9947aec03783
DespoTheAnimal <thedanishmagnus@gmail.com>
2026-02-20 14:22:01 +0100

    ssh key change in provision_keys.sh

--- vagrant/provision_keys.sh
+++ vagrant/provision_keys.sh
@@ -2,8 +2,7 @@
 
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJJ6uOxzj07JWt/8aqbwkH3qMIu615PfU0ZvZCHkovi magnusbergstedt@Mac" >> /root/.ssh/authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6lS3hLYIvcWHKP3zsh2K6SZBOQJWNwBQspdptT8/Fq mono@monolith" >> /root/.ssh/authorized_keys
-# CHANGE THIS TO THE NEW SSH
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOnNkEKfn3FGWFV4W8X39RzFMtF9bYUATHtHGpINPIH3 luismilanengel@Luis-MacBook-Air.local" >> /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPE68oskLFw2fC14xIWiZSIA0veODVn42LeswDDWI0R1 luismilanengel@Luis-MacBook-Air.local" >> /root/.ssh/authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmDcJ55KcT1ZRW5200BqocDh8kO3jq6xOzXdEZNCl9V conta@MSI" >> /root/.ssh/authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2cG2XUbHvk8i7Hmv4ix2KTiM5GAKEbJltl3pvNmCpE jbul@itu.dk" >> /root/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZmlyxqe1cHR+mt1bvPTK/qy0xhaW/AuCjeq9SvfOi

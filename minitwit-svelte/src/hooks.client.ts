f5cf2edc8475c8b66241da71edaad777a20c9c8f
Corbijn Bulsink <jbul@itu.dk>
2026-04-17 14:37:24 +0200

    fuck codacy

--- minitwit-svelte/src/hooks.client.ts
+++ minitwit-svelte/src/hooks.client.ts
@@ -6,6 +6,7 @@ window.fetch = async (input, init = {}) => {
 
     const url = typeof input === 'string' ? input : input instanceof Request ? input.url : '';
     if (response.status === 401 && !url.includes('/web/auth/session')) {
+        // eslint-disable-next-line security/detect-possible-timing-attacks
         window.location.href = '/login';
     }
 

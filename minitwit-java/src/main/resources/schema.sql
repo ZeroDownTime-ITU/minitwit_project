75e2197138a7d3535208fec3b1c1012deaa1fc45
Ymir Arnarson <ymar@itu.dk>
2026-04-19 21:46:55 +0200

    added guard so users can't follow themselves and the index on whom in the schema.sql

--- minitwit-java/src/main/resources/schema.sql
+++ minitwit-java/src/main/resources/schema.sql
@@ -28,3 +28,4 @@ CREATE UNIQUE INDEX IF NOT EXISTS idx_users_username ON users (username);
 CREATE INDEX IF NOT EXISTS idx_message_flagged_pubdate ON message (flagged, pub_date DESC);
 CREATE INDEX IF NOT EXISTS idx_message_author_pubdate ON message (author_id, pub_date DESC);
 CREATE INDEX IF NOT EXISTS idx_follower_who_id ON follower(who_id); 
+CREATE INDEX IF NOT EXISTS idx_follower_whom_id ON follower(whom_id);
